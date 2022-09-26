9e491230fba34e9005fba85595757931019743fa
Aditya-cedcoss <adityasharma2022sep@cedcoss.com>
2022-09-26 11:37:54 +0530

    JS Variables Exercise 4

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,11 @@
   document.getElementById("demo").innerHTML = x + y;
   //   JS Variables Exercise 3
 }
+{
+  // Create a variable called z, assign x + y to it, and display the result in an alert box.
+  var x = 5;
+  var y = 10;
+  var z = x + y;
+  alert(z);
+  //   JS Variables Exercise 4
+}
