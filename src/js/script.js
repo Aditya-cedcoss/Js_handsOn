908ad5d04c8f61b6009ccae58602accf14b8f2ae
Aditya-cedcoss <adityasharma2022sep@cedcoss.com>
2022-09-26 12:31:56 +0530

    JS String Method Exercise 2

--- src/js/script.js
+++ src/js/script.js
@@ -173,3 +173,9 @@
   txt = txt.touppercase();
   // JS String Method Exercise 1
 }
+{
+  // Use the slice method to return the word "bananas".
+  let txt = "I can eat bananas all day";
+  let x = txt.slice(10, 17);
+  // JS String Method Exercise 2
+}
