ac1a7cac058b0f18e98ee7eca6b86651accf9c60
Aditya-cedcoss <adityasharma2022sep@cedcoss.com>
2022-09-26 12:06:30 +0530

    JS Function Exercise 4

--- src/js/script.js
+++ src/js/script.js
@@ -96,3 +96,10 @@
   document.getElementById("demo").innerHTML = myFunction();
   // JS Function Exercise 3
 }
+{
+  // Make the function display "Hello" in the inner HTML of an element with the ID "demo".
+  function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello";
+  }
+  // JS Function Exercise 4
+}
