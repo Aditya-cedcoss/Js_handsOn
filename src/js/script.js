6939990345e63bfe8c4418eeed78769c998b8205
Aditya-cedcoss <adityasharma2022sep@cedcoss.com>
2022-09-26 11:58:25 +0530

    JS Operator Exercise 6

--- src/js/script.js
+++ src/js/script.js
@@ -62,3 +62,13 @@
   x *= y;
   // JS Operators Exercise 5
 }
+{
+  // Use comments to describe the correct data type of the following variables:
+  let length = 16; //Number
+  let lastName = "Johnson"; //String
+  const x = {
+    firstName: "John",
+    lastName: "Doe",
+  }; //Object
+  //  JS operator Exercise 6
+}
