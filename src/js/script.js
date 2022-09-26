946e8eae00ce495e321d400d217d8d4eedf905ba
Aditya-cedcoss <adityasharma2022sep@cedcoss.com>
2022-09-26 12:12:26 +0530

    JS Objects Exercise 3

--- src/js/script.js
+++ src/js/script.js
@@ -118,7 +118,17 @@
   const person = {
     firstName: "John",
     lastName: "Doe",
-    country: Norway,
+    country: "Norway",
   };
   // JS Objects Exercise 2
+}
+{
+  // Create an object called person with name = John, age = 50.
+  // Then, access the object to alert("John is 50").
+  const person = {
+    name: "John",
+    age: 50,
+  };
+  alert(person.name + " is " + person.age);
+  // JS Objects Exercise 3
 }
