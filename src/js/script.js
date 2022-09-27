6ad969ef9fc422692cce2cf87b0b1f61a8f7ac35
Aditya-cedcoss <adityasharma2022sep@cedcoss.com>
2022-09-27 12:13:20 +0530

    JS Comparison ex 2

--- src/js/script.js
+++ src/js/script.js
@@ -290,3 +290,10 @@
   alert(x > y);
   // JS Comparison ex 1
 }
+{
+  // Choose the correct comparison operator to alert true, when x is equal to y.
+  x = 10;
+  y = 10;
+  alert(x == y);
+  // JS Comparison ex 2
+}
