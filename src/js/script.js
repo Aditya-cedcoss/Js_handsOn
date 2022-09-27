822ce0a41051857ccfd1978a42cdc74bffebf7fd
Aditya-cedcoss <adityasharma2022sep@cedcoss.com>
2022-09-27 12:25:11 +0530

    JS Switch ex 2

--- src/js/script.js
+++ src/js/script.js
@@ -342,3 +342,18 @@
   }
   // JS Switch ex 1
 }
+{
+  // Add a section that will alert("Neither") if fruits is neither "banana" nor "apple".
+  switch(fruits) {
+    case "Banana":
+      alert("Hello")
+      break;
+    case "Apple":
+      alert("Welcome")
+      break;
+    
+  default:
+      alert("Neither");
+  }
+  // JS Switch ex 2
+}
