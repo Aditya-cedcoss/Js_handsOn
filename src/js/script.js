43e9eac07382f3d8e9c6d18e6456c97d7a07280c
Aditya-cedcoss <adityasharma2022sep@cedcoss.com>
2022-09-27 11:56:20 +0530

    JS array method Exercise 2

--- src/js/script.js
+++ src/js/script.js
@@ -221,4 +221,10 @@
   fruits.pop();
   // JS array method Exercise 1
 }
+{
+  // Use the correct Array method to add "Kiwi" to the fruits array.
+  const fruits = ["Banana", "Orange", "Apple"];
+  fruits.push("Kiwi");
+  // JS array method Exercise 2
+}
 
