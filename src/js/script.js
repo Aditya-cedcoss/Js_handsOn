53afdbaf2963c3f68549d127a0d891a75ba11b58
Aditya-cedcoss <adityasharma2022sep@cedcoss.com>
2022-09-27 12:22:21 +0530

    JS Switch ex 1

--- src/js/script.js
+++ src/js/script.js
@@ -326,5 +326,19 @@
   } else {
     alert("Goodbye");
   }
-  // JS Condition Ex 1
+  // JS Condition Ex 2
+}
+{
+  // Create a switch statement that will alert "Hello" if fruits is "banana", and "Welcome" if fruits is "apple".
+
+  switch (fruits) {
+    case "Banana":
+      alert("Hello");
+      break;
+
+    case "Apple":
+      alert("Welcome");
+      break;
+  }
+  // JS Switch ex 1
 }
